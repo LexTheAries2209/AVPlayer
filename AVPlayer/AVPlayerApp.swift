20bcb810640e04ff680d2c3ed485f740ab4ef3ef
LexTheAries2209 <673661658@qq.com>
2024-01-31 22:51:25 +0800

    Initial Commit

---
+++ AVPlayer/AVPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVPlayerApp.swift
+//  AVPlayer
+//
+//  Created by 吴坤城 on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct AVPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AVPlayer
+//
+//  Created by 吴坤城 on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
